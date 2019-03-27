5cf7e71799313f0f1c0c468d9928bd05413bbb87
yan Paiva <gckael@gmail.com>
2019-03-27 19:59:41 -0300

    add controller inicial

---
+++ src/main/java/br/ufjf/dcc193/exemplo01/HomeController.java
@@ -0,0 +1,13 @@
+package br.ufjf.dcc193.exemplo01;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+    @RequestMapping("index.html")
+    String home(){
+        return "/WEB-INF/home.jsp";
+    }
+    
+}
