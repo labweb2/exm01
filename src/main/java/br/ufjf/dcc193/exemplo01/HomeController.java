26ea940b91e15b2d8fa55fd9069cb94ab828200b
yan Paiva <gckael@gmail.com>
2019-03-27 20:10:51 -0300

    Altera organização das views

--- src/main/java/br/ufjf/dcc193/exemplo01/HomeController.java
+++ src/main/java/br/ufjf/dcc193/exemplo01/HomeController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class HomeController {
     @RequestMapping("index.html")
     String home(){
-        return "/WEB-INF/home.jsp";
+        return "home";
     }
     
 }
--- src/main/webapp/WEB-INF/views/home.jsp
+++ src/main/webapp/WEB-INF/views/home.jsp
@@ -1,3 +1,4 @@
+<%@page pageEncoding="utf-8"%>
 <!DOCTYPE html>
 <html>
 <head>
@@ -10,5 +11,6 @@
 </head>
 <body>
     <h1>EXM01: home</h1>
+    Introdução
 </body>
 </html>
